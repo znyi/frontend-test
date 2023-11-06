3dca5f5ea4489a9aab9c6dbf14f8b9313dd45f3a
Xinyi Wong <xinyi2709@gmail.com>
2023-11-06 11:23:23 +0900

    add steps until before pdo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,153 +11,230 @@ const defaultStopBits = 1
 const defaultFlowControl = 'none'
 
 function App() {
-    const [port, setPort] = useState(null)
+  
+  //port 
+  const [port, setPort] = useState(null)
 
-    const [isReading, setIsReading] = useState(false)
-    const readableStreamClosed = useRef(null)
-    const reader = useRef(null)
+  const [isReading, setIsReading] = useState(false)
+  const readableStreamClosed = useRef(null)
+  const reader = useRef(null)
 
-    const [pdoOutputContent, setPdoOutputContent] = useState('')
-    const pdoOutputAccumulated = useRef('')
-    
-    const [isPdoExec, setIsPdoExec] = useState(false)
+  //input output 
+  const [aInput, setAInput] = useState('')
+  const [bInput, setBInput] = useState('')
+  const [cInput, setCInput] = useState('')
 
-    async function handleConnection(){
-        if (port === null) {
-            try {
-                setPort(await navigator.serial.requestPort())
-            } catch (err) {
-                console.log(`error in requestPort: no port is chosen.\n\nerror message:\n${err}`)
-            }
-        } else {
-            setSdoInput('')
-            //stop reading port
-            await handleStopReadingPort()
-            try {
-                await port.close()
-                setPort(null)
-                alert(`disconnected \n
-                     vendor id: ${port.getInfo().usbVendorId} \n
-                     product id:  ${port.getInfo().usbProductId}`)
-            } catch (err) {
-                console.log(`error in handleConnection: ${err}`)
-            }
-        }
-    }
+  const [aOutput, setAOutput] = useState('')
+  const [bOutput, setBOutput] = useState('')
+  const [cOutput, setCOutput] = useState('')
 
-    useEffect(() => {
-      async function handleReadPort(){
-        setIsReading(true)
-        var lineBreakTransformStream = new TransformStream(new LineBreakTransformer());
-        readableStreamClosed.current = port.readable.pipeTo(lineBreakTransformStream.writable);
-        reader.current = lineBreakTransformStream.readable.getReader()
-        
-        try {
-            while (true) {
-                const { value, done } = await reader.current.read()
-                if (done) {
-                  // |reader| has been canceled.
-                  reader.current.releaseLock()
-                  break
-                }
-                readData(value)
-            }
-        } catch (err){
-            console.log(`error in handleReadPort: ${err}`)
-        }
-    }
-    async function readData(value){
-      console.log('i read')
-      console.log(value)
-
-      const SDO = Uint8Array.from([2, 59, 1])
-      const PDO = Uint8Array.from([3, 49, 2])
-
-      const commandEquals = (command, word)=>
-      command.every((elem, index) => elem === word[index])
-
-      if(commandEquals(SDO, value)){
-        setSdoOutput(await extractSdoOutput(value))
-      }
-      else if(commandEquals(PDO, value)){
-        const pdo_output_object = await extractPdoOutput(value)
-        pdoOutputAccumulated.current += `data1 = ${pdo_output_object.data1}, data2 = ${pdo_output_object.data2}\n`
-        setPdoOutputContent(pdoOutputAccumulated.current)
-      }
-    }
-    async function extractSdoOutput(value){
-      const sdo_buffer = value
-      const sdo_array = Array.from(sdo_buffer)
-      const sdo_output_buffer = JSON.stringify(sdo_array)
-      const query = `/sdo/output?sdo_output_buffer=${sdo_output_buffer}`
-      
-      const response = await fetch(BACK_END_BASE_URL+query)
-      const a_inc = await response.json()
-      return a_inc
-    }
-    async function extractPdoOutput(value){
-      const pdo_buffer = value
-      const pdo_array = Array.from(pdo_buffer)
-      const pdo_output_buffer = JSON.stringify(pdo_array)
-      const query = `/pdo/output?pdo_output_buffer=${pdo_output_buffer}`
-      
-      const response = await fetch(BACK_END_BASE_URL+query)
-      const pdo_output_object = await response.json()
-      return pdo_output_object
-    }
-    async function tryOpenPort(){
-        if (port !== null) {
-          try{
-            const portOpenOption = {
-                baudRate: defaultBaudRate,
-                dataBits: defaultDataBits,
-                stopBits: defaultStopBits,
-                bufferSize: defaultBufferSize,
-                parity: defaultParity,
-                flowControl: defaultFlowControl,
-            };
-            await port.open(portOpenOption);
-            alert(`connected \n
-                vendor id: ${port.getInfo().usbVendorId} \n
-                product id:  ${port.getInfo().usbProductId}`);
-            //read
-            await handleReadPort()
+  //port
+  async function handleConnection(){
+      if (port === null) {
+          try {
+              setPort(await navigator.serial.requestPort())
           } catch (err) {
-            console.log(err)
-            alert(`Failed to open serial port.\nThe port might be already open, or there might be something wrong with the device.`)
-            setPort(null)
+              console.log(`error in requestPort: no port is chosen.\n\nerror message:\n${err}`)
           }
+      } else {
+          try {
+              await port.close()
+              setPort(null)
+              setAInput('')
+              setBInput('')
+              setCInput('')
+              setAOutput('')
+              setBOutput('')
+              setCOutput('')
+              alert(`disconnected \n
+                   vendor id: ${port.getInfo().usbVendorId} \n
+                   product id:  ${port.getInfo().usbProductId}`)
+          } catch (err) {
+              console.log(`error in handleConnection: ${err}`)
+          }
+      }
+  }
+  async function handleReadPort(){
+    setIsReading(true)
+    var lineBreakTransformStream = new TransformStream(new LineBreakTransformer());
+    readableStreamClosed.current = port.readable.pipeTo(lineBreakTransformStream.writable);
+    reader.current = lineBreakTransformStream.readable.getReader()
+    try {
+      const { value, done } = await reader.current.read()
+      setIsReading(false)
+      reader.current.cancel();
+      await readableStreamClosed.current.catch(() => { /* Ignore the error */ }); 
+      return value
+    } catch (err){
+        console.log(`error in handleReadPort: ${err}`)
+    }
+  }
+  
+  //input output
+  function handleChangeInput({target}){
+    if (target.name === 'aInput') {
+      setAInput(target.value)
+    } else if (target.name === 'bInput') {
+      setBInput(target.value)
+    } else if (target.name === 'cInput') {
+      setCInput(target.value)
+    } 
+  }
+
+  async function handleApplySine(){
+    const inputDataset = {a:aInput, b:bInput, c:cInput}
+    console.log(inputDataset)
+
+    //step1
+    const query = `/sine/step/1`
+    var variables = {
+      variables: {
+        a: parseInt(aInput)
+      }
+    }
+    var options = {
+      method:'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(variables)
+    }
+    var buffer = []
+    try{
+      const response = await fetch(BACK_END_BASE_URL+query, options)
+      const responseJSON = await response.json()
+      variables = responseJSON.variables
+      await handleWritePort(responseJSON.buffer)
+      const readval = await handleReadPort()
+      buffer =  Array.from(readval)
+    } catch (err) {
+      console.log(err)
+    }
+console.log('done step1')
+    //step2
+    const query2 = `/sine/step/2`
+    
+    variables = {
+      variables: {
+        ...variables,
+        b: parseInt(bInput)
+      },
+      buffer: buffer
+    }
+    options = {
+      method:'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(variables)
+    }
+    try {
+      const response2 = await fetch(BACK_END_BASE_URL+query2, options)
+      var responseJSON2 = await response2.json()
+      variables = responseJSON2.variables
+      await handleWritePort(responseJSON2.buffer)
+      const readval2 = await handleReadPort()
+      buffer =  Array.from(readval2)
+    } catch (err) {
+      console.log(err)
+    }
+console.log('done step2')
+    //step3
+    const query3 = `/sine/step/3`
+    variables = {
+      variables: {
+        ...variables,
+        c: parseInt(cInput)
+      },
+      buffer: buffer
+    }
+    options = {
+      method:'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(variables)
+    }
+    try{
+      const response3 = await fetch(BACK_END_BASE_URL+query3, options)
+      var responseJSON3 = await response3.json()
+      variables = responseJSON3.variables
+      await handleWritePort(responseJSON3.buffer)
+      const readval3 = await handleReadPort()
+      buffer =  Array.from(readval3)
+    } catch (err) {
+      console.log(err)
+    }
+console.log('done step3')
+    //step4
+    const query4 = `/sine/step/4`
+    variables = {
+      variables: {
+        ...variables
+      },
+      buffer: buffer
+    }
+    options = {
+      method:'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(variables)
+    }
+    try{
+      const response4 = await fetch(BACK_END_BASE_URL+query4, options)
+      var responseJSON4 = await response4.json()
+      variables = responseJSON4.variables
+      console.log(responseJSON4)
+      await handleWritePort(responseJSON4.buffer)
+      const readval4 = await handleReadPort() //lots of data coming since this
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    async function tryOpenPort(){
+      if (port !== null) {
+        try{
+          const portOpenOption = {
+              baudRate: defaultBaudRate,
+              dataBits: defaultDataBits,
+              stopBits: defaultStopBits,
+              bufferSize: defaultBufferSize,
+              parity: defaultParity,
+              flowControl: defaultFlowControl,
+          };
+          await port.open(portOpenOption);
+          alert(`connected \n
+              vendor id: ${port.getInfo().usbVendorId} \n
+              product id:  ${port.getInfo().usbProductId}`);
+        } catch (err) {
+          console.log(err)
+          alert(`Failed to open serial port.\nThe port might be already open, or there might be something wrong with the device.`)
+          setPort(null)
         }
       }
-      tryOpenPort()
-    }, [port])
-
-    async function handleStopReadingPort(){
-        if(isReading){
-            setIsReading(false)
-            setSdoOutput('')
-            setPdoOutputContent('')
-            reader.current.cancel();
-            await readableStreamClosed.current.catch(() => { /* Ignore the error */ }); 
-        }
     }
+    tryOpenPort()
+  }, [port])
 
-  const [sdoInput, setSdoInput] = useState('')
-  function handleChangeSdoInput({target}){
-    setSdoInput(target.value)
+  async function handleStopReadingPort(){
+      if(isReading){
+          setIsReading(false)
+          reader.current.cancel();
+          await readableStreamClosed.current.catch(() => { /* Ignore the error */ }); 
+      }
   }
 
-  const [sdoOutput, setSdoOutput] = useState('')
-  function handleChangeSdoOutput({target}){
-    setSdoOutput(target.value)
-  }
-
-  async function handleWritePort(bufToWrite){
+  async function handleWritePort(bufferToWrite){
     const writer = port.writable.getWriter()
     try {
-        var mybuf = new Uint8Array(bufToWrite)
-        console.log('i write')
-        console.log(mybuf)
+        var mybuf = new Uint8Array(bufferToWrite)
         await writer.write(mybuf)
     } catch (err) {
         console.log(`error in handleWritePort: ${err}`)
@@ -166,60 +243,47 @@ function App() {
     }
 }
 
-async function handleSdo(){
-  const a = parseInt(sdoInput)
-  const query = `/sdo/command?sdo_a=${a}`
-  const response = await fetch(BACK_END_BASE_URL+query)
-  const bufferToWrite = await response.json()
-  handleWritePort(bufferToWrite)
-}
-
-async function handlePdoExec(){
-  setIsPdoExec(true)
-  const query = `/pdo/command/exec`
-  const response = await fetch(BACK_END_BASE_URL+query)
-  const bufferToWrite = await response.json()
-  handleWritePort(bufferToWrite)
-}
-async function handlePdoStop(){
-  setIsPdoExec(false)
-  const query = `/pdo/command/stop`
-  const response = await fetch(BACK_END_BASE_URL+query)
-  const bufferToWrite = await response.json()
-  handleWritePort(bufferToWrite)
-}
-  
   return (
     <div className="App">
         <div className="container">
             <div className="part">
-                <h3>connect and read from port</h3>
+                <h3>connect</h3>
                 <div>
                   <button onClick={handleConnection} >{port!==null? 'disconnect':'connect'}</button>
+                  <button disabled={port!==null? false:true}>{isReading? 'is reading':'not reading'}</button>
                 </div>
             </div>
             <div className="part">
                 <h3>SDO test</h3>
                 <div>
                   <div>
-                    <label htmlFor='sdoInput'>a</label>
-                    <input name='sdoInput' type='text' disabled={port!==null? false:true} placeholder={port!==null? "input int between 0~254":"port is not connected"} value={sdoInput} onChange={handleChangeSdoInput}></input>
-                    <button onClick={handleSdo} disabled={port!==null? false:true}>SDO</button>
+                    <label htmlFor='aInput'>A</label>
+                    <input name='aInput' type='text' disabled={port!==null? false:true} placeholder={port!==null? "":"port is not connected"} value={aInput} onChange={handleChangeInput}></input>
                   </div>
                   <div>
-                    <label htmlFor='sdoOutput'>a+1</label>
-                    <input name='sdoOutput' type='text' disabled={true} placeholder={port!==null? "":"port is not connected"} value={sdoOutput} onChange={handleChangeSdoOutput}></input>
+                    <label htmlFor='bInput'>B</label>
+                    <input name='bInput' type='text' disabled={port!==null? false:true} placeholder={port!==null? "":"port is not connected"} value={bInput} onChange={handleChangeInput}></input>
+                  </div>
+                  <div>
+                    <label htmlFor='cInput'>C</label>
+                    <input name='cInput' type='text' disabled={port!==null? false:true} placeholder={port!==null? "":"port is not connected"} value={cInput} onChange={handleChangeInput}></input>
+                  </div>
+                  <div>
+                  <button onClick={handleApplySine} disabled={port!==null? false:true}>apply</button>
+                  </div>
+                  <div>
+                    <label htmlFor='aOutput'>A'</label>
+                    <input name='aOutput' type='text' disabled={true} placeholder={port!==null? "":"port is not connected"} value={aOutput}></input>
+                  </div>
+                  <div>
+                    <label htmlFor='bOutput'>B'</label>
+                    <input name='bOutput' type='text' disabled={true} placeholder={port!==null? "":"port is not connected"} value={bOutput}></input>
+                  </div>
+                  <div>
+                    <label htmlFor='cOutput'>C'</label>
+                    <input name='cOutput' type='text' disabled={true} placeholder={port!==null? "":"port is not connected"} value={cOutput}></input>
                   </div>
                 </div>
-            </div>
-            <div className="part">
-              <h3>PDO test</h3>
-              <div>
-                <button onClick={!isPdoExec? handlePdoExec:handlePdoStop} disabled={port!==null? false:true}>{!isPdoExec? 'exec':'stop'}</button>
-              </div>
-              <div>
-                <textarea value={pdoOutputContent} disabled={true} placeholder={port!==null? (isReading? pdoOutputContent:''):"port is not connected"}></textarea>
-              </div>
             </div>
         </div>
     </div>
